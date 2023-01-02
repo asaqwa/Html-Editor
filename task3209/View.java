b14ef3131d5754a9cefcbd6d3fd150afb673d324
Andrey <bursov@web.de>
2023-01-02 22:05:18 +0100

    Task six complete

--- task3209/View.java
+++ task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -41,7 +43,22 @@ public class View extends JFrame implements ActionListener {
 
     public void initMenuBar() {}
 
-    public void initEditor() {}
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+
+        JScrollPane htmlScroll = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", htmlScroll);
+
+        JScrollPane plainScroll = new JScrollPane(plainTextPane);
+        tabbedPane.add("Текст", plainScroll);
+
+        tabbedPane.setPreferredSize(new Dimension(700, 400));
+
+        TabbedPaneChangeListener changeListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(changeListener);
+
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
 
     public void initGui() {
         initMenuBar();
