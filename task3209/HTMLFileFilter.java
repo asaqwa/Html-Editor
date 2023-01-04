9fcf2ea6b56e6892a501036e1e0f3b0c4cf9535f
Andrey <bursov@web.de>
2023-01-04 20:58:58 +0100

    Task twenty one complete

---
+++ task3209/HTMLFileFilter.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public class HTMLFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File f) {
+        if (f.isDirectory()) return true;
+        String file = f.getName().toLowerCase();
+        return file.endsWith(".html") || file.endsWith(".htm");
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}
